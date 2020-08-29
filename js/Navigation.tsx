70db1ec3bbf283a78ddf9d98cbb6c3782ccec620
Daniel Caris <daniel.caris22@gmail.com>
2020-08-29 18:20:30 -0400

    :sparkles: Add Navigation

---
+++ js/Navigation.tsx
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
